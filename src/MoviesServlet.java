d20a09d5e3ce2b6acc991aa82c623bf1360935d7
jasontran320 <jasont16@uci.edu>
2025-02-27 13:04:07 -0800

    Task 4 configuration commit

--- src/MoviesServlet.java
+++ src/MoviesServlet.java
@@ -26,7 +26,7 @@ public class MoviesServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/MySQLReadOnly");
         } catch (NamingException e) {
             e.printStackTrace();
         }
--- src/PlaceOrderServlet.java
+++ src/PlaceOrderServlet.java
@@ -23,7 +23,7 @@ public class PlaceOrderServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/MySQLReadWrite");
         } catch (NamingException e) {
             e.printStackTrace();
         }
